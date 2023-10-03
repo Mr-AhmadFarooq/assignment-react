968ef1f6a3a200a448e110b2bfdf5bd3ce38cdbb
Mr-AhmadFarooq <146879204+Mr-AhmadFarooq@users.noreply.github.com>
2023-10-04 00:53:50 +0500

    this is assignment
    this is assignment

---
+++ app.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <div className="card__image">
+          <img src="https://images.unsplash.com/photo-1521139869420-edaae1bc7b9a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ" alt="abc" />
+        </div>
+        <div className="card__copy">
+          <h1>Swat</h1>
+          <h2>Swat District, also known as the swat</h2>
+          <p>
+            Swat District also known as the Swat Valley, is a district in the Malakanda division of Khyber Pakhtunkhwa, Pakistan. With a population of 2,309,570 per the 2017 national census, Swat is the 15th-largest district of Khyber Pakhtunkhwa with many popular tourist attractions.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
